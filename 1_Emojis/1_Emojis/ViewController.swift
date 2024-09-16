f392f01ec04de9782343ca857be485333a13dec4
“Raffatoo” <rtorres_33@hotmail.com>
2024-09-16 07:06:55 -0600

    Conociendo Interfaz Builder App

---
+++ 1_Emojis/1_Emojis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1_Emojis
+//
+//  Created by Rafael Torres on 15/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
