b1ed164b2f6eab2402f8c31d62d3ed98eb91dc04
“Raffatoo” <rtorres_33@hotmail.com>
2024-09-16 07:08:23 -0600

    Diseño automatico, tamaño, posición.

---
+++ 2_AutoLayout/2_AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2_AutoLayout
+//
+//  Created by Rafael Torres on 16/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
